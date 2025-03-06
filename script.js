542905dccec836f8f9bdeebcec248f48f91617b8
Gabriel <gabrieltux19@gmail.com>
2025-03-06 20:52:05 -0300

    Primeira funcionalidade em JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+const seletor = document.getElementById("deslocamento")
+
+for (let i = 0; i < alfabeto.length; i++){
+    seletor.innerHTML = seletor.innerHTML +`<option value="${i}">${alfabeto[i]}</option>`
+}
